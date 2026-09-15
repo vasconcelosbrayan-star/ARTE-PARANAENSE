6bcf982133f2f6d2191089d942c82b6f8096510d
BRAYAN CUIUDOSEFODA <vasconcelos.brayan@escola.pr.gov.br>
2026-09-15 11:22:10 -0300

    atualizei tdos os cod

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-
+const botoesCurtir = document.querySelectorAll(".curtir");
+botoesCurtir.ForEach(Fuction(botaoCurtir){
+    let  curtiu = false;
+    botaocurtir.addEventlistener("click", curtir);
+    function curtir (){
+        const contador = botaoCurtir.queryselector("span");
+        if(curtiu === false){
+            contador.textContent++;0
+            curtiu + true;}
+else {
+    contador.textContent--;
+    curtiu= false;
+           }
+}
+});
